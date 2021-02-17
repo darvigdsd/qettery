4bde25e365d4efedfdfba4737d3b42835f499a9f
darvigdsd <d4vid4m234@gmail.com>
2021-02-18 02:27:51 +0500

    new version Qettery
    Beta 2!

--- scripts/activate-client.js
+++
@@ -1,8 +0,0 @@
-
-
-var activatebtn = document.getElementById('activateclient');
-
-activatebtn.onclick = function() {
-    localStorage.setItem('webclient', 'active');
-    document.location.href = 'client/home.html';
-}
